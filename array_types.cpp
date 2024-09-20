f1a7b99a0468ad67db8df02634cbe71d5fbbcbd0
harshkumar-2005 <157991814+harshkumar-2005@users.noreply.github.com>
2024-09-21 00:35:18 +0530

    types of arra intro

---
+++ array_types.cpp
@@ -0,0 +1,20 @@
+// types of an array:-
+// fixed sized array 
+// int arr[10]
+// int arr[n]
+// int *arr = new int [n]
+//int arr[] = {10,15,34,56,45,34,50}
+// dynamic sized array
+// resized automatically 
+// c++:- vector
+
+//that all for this file
+
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+    cout<<"hello, world";
+    return 0;
+}
